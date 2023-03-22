48637fe1bab06e4cec9f53d6e315810a8c215abf
Abhishek <abhishek.pandey@ucertify.com>
2023-03-22 11:22:16 +0530

    commit message

--- index.php
+++ index.php
@@ -1,23 +1,11 @@
 <?php
 /**
- * File-level documentation comment.
- *
- * This file contains the Foo class and a function to add two numbers.
- */
-
-class Foo {
-     public function bar() {
-          echo 'Hello World!';
-     }
-}
-
-/**
  * Add two numbers.
  *
  * @param  int $a The first number to add.
  * @param  int $b The second number to add.
  * @return int The sum of the two numbers.
  */
-function add( $a, $b ) {
+function add($a, $b) {
      return $a + $b;
 }
