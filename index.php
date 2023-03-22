12338187adec7abfbb1c4ff87131c083a9264da2
Abhishek <abhishek.pandey@ucertify.com>
2023-03-22 11:19:46 +0530

    commit message

--- index.php
+++ index.php
@@ -2,18 +2,22 @@
 /**
  * File-level documentation comment.
  *
- * This file contains code that does XYZ.
- *
- * @author John Doe
+ * This file contains the Foo class and a function to add two numbers.
  */
-class Foo {
 
-     ublic function bar() {
+class Foo {
+     public function bar() {
           echo 'Hello World!';
      }
 }
 
-// add two numbers
+/**
+ * Add two numbers.
+ *
+ * @param  int $a The first number to add.
+ * @param  int $b The second number to add.
+ * @return int The sum of the two numbers.
+ */
 function add( $a, $b ) {
      return $a + $b;
 }
